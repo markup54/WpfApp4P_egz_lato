bdc0ddb0396df3c782fedd8595db358bcdab07fb
markup54 <marzenakupka@tlen.pl>
2023-09-28 08:08:50 +0200

    gotowe zadanie

--- WpfApp4P_egz_lato/MainWindow.xaml.cs
+++ WpfApp4P_egz_lato/MainWindow.xaml.cs
@@ -30,14 +30,45 @@ namespace WpfApp4P_egz_lato
             if(list.IsChecked == true)
             {
                 obrazekimg.Source = new BitmapImage(new Uri(@"obrazki/list.png",UriKind.Relative));
-
+                cenatxt.Text = "Cena: 1,5 zł";
             }
             else
             {
                 if(pocztowka.IsChecked == true) {
                     obrazekimg.Source = new BitmapImage(new Uri(@"obrazki/pocztowka.png", UriKind.Relative));
+                    cenatxt.Text = "Cena: 1 zł";
+                }
+                else
+                    if(paczka.IsChecked == true)
+                {
+                    obrazekimg.Source = new BitmapImage(new Uri(@"obrazki/paczka.png", UriKind.Relative));
+                    cenatxt.Text = "Cena: 10 zł";
                 }
             }
+        }
+
+        private void Zatwierdz_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (kodtxtbox.Text.Length != 5)
+            {
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+            }
+            else
+            {
+                int wynik;
+                if(int.TryParse(kodtxtbox.Text,out wynik))
+                {
+                    if(wynik>=0 && wynik <= 99999)
+                    {
+                        MessageBox.Show("Dane przesyłki zostały wprowadzone");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Kod pocztowy powinien się skłądać z samych cyfr");
+                }
+            }
+            
         }
     }
 }
